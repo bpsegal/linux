cefa4419f659155d44d458d1b5de9878c97604c0
Ben Segal <bpsegal20@gmail.com>
2019-07-29 11:13:52 -0400

    Update update-hl script to also unload and reload the habanalabs driver as part of the update process.

--- update-hl.sh
+++ update-hl.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-HL_VERSION=5.2.0+
-HL_MODNAME=habanalabs.ko
-SRC_HL_PATH=drivers/misc/habanalabs/$HL_MODNAME
-TGT_HL_PATH=/lib/modules/$HL_VERSION/kernel/drivers/misc/habanalabs/$HL_MODNAME
+HL_VERSION=$(uname -r)
+HL_MODNAME=habanalabs
+HL_KSUFFIX=.ko
+SRC_HL_PATH=drivers/misc/habanalabs/$HL_MODNAME$HL_KSUFFIX
+TGT_HL_PATH=/lib/modules/$HL_VERSION/kernel/drivers/misc/habanalabs/$HL_MODNAME$HL_KSUFFIX
+CMD="sudo modprobe -r $HL_MODNAME"
+echo $CMD
+eval $CMD
 CMD="sudo cp -p $SRC_HL_PATH $TGT_HL_PATH"
 echo $CMD
 eval $CMD
 CMD="stat $TGT_HL_PATH"
+echo $CMD
+eval $CMD
+CMD="sudo modprobe $HL_MODNAME"
 echo $CMD
 eval $CMD
